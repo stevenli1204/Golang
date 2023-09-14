910ead53a2a45cc0d16020151abc985da7b14fc5
Swanhello <swanhello@hotmail.com>
2023-09-14 18:13:08 +1000

    Update http-server.go

--- http-server.go
+++ http-server.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Hello, world!")
+}
